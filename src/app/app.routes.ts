974618f93f10ed0b0ef89d947d9d78618a9f13c6
jauntee <noahwj305@gmail.com>
2025-03-19 16:04:05 -0500

    Integrated details page into the application

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -8,9 +8,14 @@ export const routes: Routes = [
             component: HomeComponent,
             title: 'Home page',
     },
+//    {
+//            path: 'details',
+//            component: DetailsComponent,
+//            title: 'Home Details'
+//    },
     {
-            path: 'details',
+            path: 'details/:id',
             component: DetailsComponent,
             title: 'Home Details'
-    },
+    }
 ];
